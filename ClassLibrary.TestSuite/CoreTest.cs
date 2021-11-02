3066d18485095c74ffedba0aa3eeb954082ad2c9
namakeingo <pzzdave95@gmail.com>
2021-11-02 17:54:41 +0000

    green

--- ClassLibrary.TestSuite/CoreTest.cs
+++ ClassLibrary.TestSuite/CoreTest.cs
@@ -6,22 +6,252 @@ namespace ClassLibrary.TestSuite
     {
         Core TestObject { get; set; }
 
-        [SetUp]
-        public void Setup()
+        /// <summary>
+        /// Succesfull start of game
+        /// </summary>
+        [Test]
+        public void GameStart_Score()
         {
-            TestObject = new Core();
+            Core testObj = new Core();
+
+            Assert.True("0-0" == testObj.GetScore());
         }
 
+        /// <summary>
+        /// Succesfull test for 1 point scored by PlayerOne
+        /// </summary>
         [Test]
-        public void TestPass()
+        public void Player1_Score()
         {
-            Assert.Pass();
+            Core testObj = new Core();
+
+            testObj.PlayerOneScore();
+
+            Assert.True("15-0" == testObj.GetScore());
         }
 
+        /// <summary>
+        /// Succesfull test for 1 point scored by PlayerTwo
+        /// </summary>
         [Test]
-        public void TestFail()
+        public void Player2_Score()
         {
-            Assert.Fail();
+            Core testObj = new Core();
+
+            testObj.PlayerTwoScore();
+
+            Assert.True("0-15" == testObj.GetScore());
+        }
+
+        /// <summary>
+        /// Succesfull test for 3 point scored by PlayerOne
+        /// </summary>
+        [Test]
+        public void Player1_Score_x3()
+        {
+            Core testObj = new Core();
+
+            testObj.PlayerOneScore();
+            testObj.PlayerOneScore();
+            testObj.PlayerOneScore();
+
+            Assert.True("40-0" == testObj.GetScore());
+        }
+
+        /// <summary>
+        /// Succesfull test for 3 point scored by PlayerTwo
+        /// </summary>
+        [Test]
+        public void Player2_Score_x3()
+        {
+            Core testObj = new Core();
+
+            testObj.PlayerTwoScore();
+            testObj.PlayerTwoScore();
+            testObj.PlayerTwoScore();
+
+            Assert.True("0-40" == testObj.GetScore());
+        }
+
+        /// <summary>
+        /// Succesfull test for Deuce
+        /// </summary>
+        [Test]
+        public void Deuce()
+        {
+            Core testObj = new Core();
+
+            testObj.PlayerOneScore(); 
+            testObj.PlayerTwoScore();
+
+            testObj.PlayerOneScore();
+            testObj.PlayerTwoScore();
+
+            testObj.PlayerOneScore();
+            testObj.PlayerTwoScore();
+
+            Assert.True("Deuce" == testObj.GetScore());
+        }
+
+        /// <summary>
+        /// Succesfull test for Server wins game
+        /// </summary>
+        [Test]
+        public void ServerWinsGame()
+        {
+            Core testObj = new Core();
+
+            testObj.PlayerOneScore();
+            testObj.PlayerOneScore();
+            testObj.PlayerOneScore();
+            testObj.PlayerOneScore();
+
+            Assert.True("Server wins game" == testObj.GetScore());
+        }
+
+        /// <summary>
+        /// Succesfull test for Server loses game
+        /// </summary>
+        [Test]
+        public void ServerLosesGame()
+        {
+            Core testObj = new Core();
+
+            testObj.PlayerTwoScore();
+            testObj.PlayerTwoScore();
+            testObj.PlayerTwoScore();
+            testObj.PlayerTwoScore();
+
+            Assert.True("Server loses game" == testObj.GetScore());
+        }
+
+        /// <summary>
+        /// Succesfull test for Advantage server
+        /// </summary>
+        [Test]
+        public void AdvantageServer()
+        {
+            Core testObj = new Core();
+
+            testObj.PlayerOneScore();
+            testObj.PlayerTwoScore();
+
+            testObj.PlayerOneScore();
+            testObj.PlayerTwoScore();
+
+            testObj.PlayerOneScore();
+            testObj.PlayerOneScore();
+
+
+            Assert.True("Advantage server" == testObj.GetScore());
+        }
+
+        /// <summary>
+        /// Succesfull test for Advantage non-server
+        /// </summary>
+        [Test]
+        public void AdvantageNonServer()
+        {
+            Core testObj = new Core();
+
+            testObj.PlayerOneScore();
+            testObj.PlayerTwoScore();
+
+            testObj.PlayerOneScore();
+            testObj.PlayerTwoScore();
+
+            testObj.PlayerTwoScore();
+            testObj.PlayerTwoScore();
+
+            Assert.True("Advantage non-server" == testObj.GetScore());
+        }
+
+        /// <summary>
+        /// Succesfull test for Deuce after Advantage server
+        /// </summary>
+        [Test]
+        public void DeuceAfterAdvantageServer()
+        {
+            Core testObj = new Core();
+
+            testObj.PlayerOneScore();
+            testObj.PlayerTwoScore();
+
+            testObj.PlayerOneScore();
+            testObj.PlayerTwoScore();
+
+            testObj.PlayerOneScore();
+            testObj.PlayerOneScore();
+
+            testObj.PlayerTwoScore();
+
+            Assert.True("Deuce" == testObj.GetScore());
+        }
+
+        /// <summary>
+        /// Succesfull test for Deice after Advantage non-server
+        /// </summary>
+        [Test]
+        public void DeuceAfterAdvantageNonServer()
+        {
+            Core testObj = new Core();
+
+            testObj.PlayerOneScore();
+            testObj.PlayerTwoScore();
+
+            testObj.PlayerOneScore();
+            testObj.PlayerTwoScore();
+
+            testObj.PlayerTwoScore();
+            testObj.PlayerTwoScore();
+
+            testObj.PlayerOneScore();
+
+            Assert.True("Deuce" == testObj.GetScore());
+        }
+
+        /// <summary>
+        /// Succesfull test for Advantage server
+        /// </summary>
+        [Test]
+        public void WinAfterAdvantageServer()
+        {
+            Core testObj = new Core();
+
+            testObj.PlayerOneScore();
+            testObj.PlayerTwoScore();
+
+            testObj.PlayerOneScore();
+            testObj.PlayerTwoScore();
+
+            testObj.PlayerOneScore();
+            testObj.PlayerOneScore();
+
+            testObj.PlayerOneScore();
+
+            Assert.True("Server wins game" == testObj.GetScore());
+        }
+
+        /// <summary>
+        /// Succesfull test for Advantage non-server
+        /// </summary>
+        [Test]
+        public void LossAfterAdvantageNonServer()
+        {
+            Core testObj = new Core();
+
+            testObj.PlayerOneScore();
+            testObj.PlayerTwoScore();
+
+            testObj.PlayerOneScore();
+            testObj.PlayerTwoScore();
+
+            testObj.PlayerTwoScore();
+            testObj.PlayerTwoScore();
+
+            testObj.PlayerTwoScore();
+
+            Assert.True("Server loses game" == testObj.GetScore());
         }
     }
 }
--- ClassLibrary/Core.cs
+++ ClassLibrary/Core.cs
@@ -4,5 +4,71 @@ namespace ClassLibrary
 {
     public class Core
     {
+        private int ScoreOne { get; set; }
+        private int ScoreTwo { get; set; }
+
+        public string GetScore()
+        {
+            string result = "";
+
+            if (ScoreOne == 40 && ScoreTwo == 40
+                || ScoreOne == 50 && ScoreTwo == 40
+                || ScoreOne == 40 && ScoreTwo == 50)
+            {
+                result = "Deuce";
+            }
+            else if (ScoreOne == 50)
+            {
+                if (ScoreTwo < 30)
+                {
+                    result = "Server wins game";
+                }
+                else
+                {
+                    result = "Advantage server";
+                }
+            }
+            else if (ScoreTwo == 50)
+            {
+                if (ScoreOne < 30)
+                {
+                    result = "Server loses game";
+                }
+                else
+                {
+                    result = "Advantage non-server";
+                }
+            }
+            else if (ScoreOne == 60 && ScoreTwo == 30)
+            {
+                result = "Server wins game";
+            }
+            else if (ScoreOne == 30 && ScoreTwo == 60)
+            {
+                result = "Server loses game";
+            }
+            else
+            {
+                //For not endgame score format points as {0}-{1}
+                result = string.Format("{0}-{1}", ScoreOne, ScoreTwo);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Add a score to player one total points
+        /// </summary>
+        public void PlayerOneScore()
+        {
+            ScoreOne = ScoreOne >= 30 ? (ScoreOne + 10) : (ScoreOne + 15);
+        }
+
+        /// <summary>
+        /// Add a score to player two total points
+        /// </summary>
+        public void PlayerTwoScore()
+        {
+            ScoreTwo = ScoreTwo >= 30 ? (ScoreTwo + 10) : (ScoreTwo + 15);
+        }
     }
 }
